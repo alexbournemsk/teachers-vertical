d123a598aadc0214babffb57b43e50772507a9d5
alexbournemsk <arbiter1@yandex.ru>
2021-11-06 19:44:07 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const videoPlayer = document.querySelector('button');
+const textContent = document.querySelector('.text-content');
+const x = 5;
+//videoPlayer.addEventListener('click', alert('hello'))
+//alert('hello')
+console.log(videoPlayer);
+console.log(textContent);
+console.log(x)
